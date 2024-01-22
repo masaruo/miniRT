6c00a5ec5ff0079cc7655599c15596229f075d36
masaru ogawa <masaru@gmail.com>
2024-01-22 10:09:46 +0900

    adding shadow function

---
+++ src/shadow.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shadow.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 17:43:09 by mogawa            #+#    #+#             */
+/*   Updated: 2024/01/21 18:18:49 by mogawa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shadow.h"
+
+t_ray	get_shadow_ray(t_intersect const *intersect, t_light const *light)
+{
+	t_ray	shadow_ray;
+
+	shadow_ray.start = 
+}
